af0351b652d28a084c63e19c8124083778979164
jesus <jesusgonzalez071099@gmail.com>
2023-08-10 09:45:11 -0400

    se creo la lista de criptomonedas, se agrego axios y se creo el archivo css

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,47 @@
-import { useEffect } from "react"
+import { useState, useEffect } from "react"
+import axios from "axios"
+import "./App.css"
 
 const App = () => {
 
+const API_URL = import.meta.env.VITE_API_URL
+
+	const [criptos, setCriptos] = useState()
 	useEffect(() => {
-		fetch("https://api.coincap.io/v2/assets")
-			.then((resp) => resp.json())
+		axios.get(`${API_URL}assets`)
 			.then((data) => {
-				console.log(data)
+				setCriptos(data.data.data)
 			})
 			.catch(() => {
 				console.error("la peticion fallo")
 			})
+		/*
+		fetch(`${API_URL}assets`)
+			.then((resp) => resp.json())
+			.then((data) => {
+				setCriptos(data.data)
+			})
+			.catch(() => {
+				console.error("la peticion fallo")
+			})
+			*/
 	}, [])
+	
+	if(!criptos) return <span>Cargando...</span>
 
 	return (
-	  <h1>
+		<>
+	  	<h1>
 	 		LISTA DE CRIPTOMONEDAS
-	  </h1>
+	  	</h1>
+	  	<ol>
+	  		{
+				criptos.map(({id, name, priceUsd}) => (
+	  				<li key={id}>Nombre: {name} Precio: {priceUsd}</li>
+	  			))
+			}
+	  	</ol>
+	  </>
 	)
 }
 
