dc3ce7a64a4fcb43305fd8d4552216c4700e4891
jesus <jesusgonzalez071099@gmail.com>
2023-08-09 20:45:49 -0400

    primer commit del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useEffect } from "react"
+
+const App = () => {
+
+	useEffect(() => {
+		fetch("https://api.coincap.io/v2/assets")
+			.then((resp) => resp.json())
+			.then((data) => {
+				console.log(data)
+			})
+			.catch(() => {
+				console.error("la peticion fallo")
+			})
+	}, [])
+
+	return (
+	  <h1>
+	 		Lista de criptomonedas
+	  </h1>
+	)
+}
+
+export default App
